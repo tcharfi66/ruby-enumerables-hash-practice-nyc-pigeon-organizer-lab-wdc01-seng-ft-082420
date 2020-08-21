4e263f866c13c15bac57efcc326549414a200622
Tej Charfi <charfi@usc.edu>
2020-08-21 14:47:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,6 @@
-require 'pry'
-
-
 def nyc_pigeon_organizer(data)
-  # Iterate over the hash above, colecting each pigeon by name and insert it as the key of a new hash 
-  #each of these hashs should habe color, gender, and lives keys assigned to arrays of info about that pigeon 
+  
+  
   
   
   
